75a6e8b51369d1d27071d6b64fa3046fed42a3d5
Pirthvi Singh <pirthvi2016@gmail.com>
2023-08-31 14:24:08 +0500

    discovery server changes

--- student/src/main/java/com/studentservice/student/StudentApplication.java
+++ student/src/main/java/com/studentservice/student/StudentApplication.java
@@ -3,7 +3,9 @@ package com.studentservice.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class StudentApplication {
 
 	public static void main(String[] args) {
