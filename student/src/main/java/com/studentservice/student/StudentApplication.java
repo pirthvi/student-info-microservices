03c8ff2b41b6dcde7d8566399f520eef12c67049
Pirthvi Singh <pirthvi2016@gmail.com>
2023-08-31 07:51:57 +0500

    test

---
+++ student/src/main/java/com/studentservice/student/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.studentservice.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
